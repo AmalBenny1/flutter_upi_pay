bfbcaf2a8e9254064da9b65f5c253b9406da7be0
AmalBenny1 <117149158+AmalBenny1@users.noreply.github.com>
2024-05-10 14:09:21 +0530

    new commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_upi_pay
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterPayUpiPlugin.register(with: registry.registrar(forPlugin: "FlutterPayUpiPlugin"))
+}
